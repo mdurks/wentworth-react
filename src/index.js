c5bc83d1d998f810763fb83012cca0133ae85d0d
Michael Durkin <mdurks@gmail.com>
2020-06-28 20:14:39 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import "./style.css";
+
+const App = () => (
+  <div>
+    <h1>svideo</h1>
+  </div>
+);
+
+render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
+};
