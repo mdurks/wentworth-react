e68783e4e674cd713c7c5feb34292ecaf28ad6eb
Michael Durkin <mdurks@gmail.com>
2020-06-30 15:40:26 +0100

    Revert routing without code splitting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import loadable from "@loadable/component";
+// import loadable from "@loadable/component";
 
 import Landing from "./Landing";
+import About from "./About";
+import Contact from "./Contact";
 
 const App = () => {
   return (
@@ -11,12 +13,14 @@ const App = () => {
       <Link to="/contact">Contact</Link>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/about" component={loadable(() => import("./About"))} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        {/* <Route path="/about" component={loadable(() => import("./About"))} />
         <Route
           path="/contact"
           component={loadable(() => import("./Contact"))}
         />
-        <Route component={loadable(() => import("./FourOhFour"))} />
+        <Route component={loadable(() => import("./FourOhFour"))} /> */}
       </Switch>
     </BrowserRouter>
   );
