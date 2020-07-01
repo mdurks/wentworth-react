5f30b4e46fab95b80e17a55507f285f59e0cb545
Michael Durkin <mdurks@gmail.com>
2020-07-01 16:14:38 +0100

    Styled Components added, new pages hooked upto GraphCMS

--- src/Landing.js
+++ src/Landing.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+import styled from "styled-components";
+import MainNav from "./components/MainNav";
+import MainFooter from "./components/MainFooter";
+import Styled_SiteContainer from "./styles/commonStyles";
+
 const WELCOME_QUERY = gql`
   query {
     welcomes {
@@ -13,21 +18,31 @@ const WELCOME_QUERY = gql`
 
 const Landing = () => {
   return (
-    <Query query={WELCOME_QUERY}>
-      {({ loading, error, data }) => {
-        if (loading) return <div>Loading...</div>;
-        if (error) return <div>Error loading welcome</div>;
+    <>
+      <MainNav />
+      <Query query={WELCOME_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading)
+            return <Styled_SiteContainer>Loading...</Styled_SiteContainer>;
+          if (error)
+            return (
+              <Styled_SiteContainer>Error loading welcome</Styled_SiteContainer>
+            );
 
-        const items = data.welcomes[0];
-        // console.log(items);
-        return (
-          <>
-            <h1>{items.heroHeading}</h1>
-            <p>{items.firstIntroMessage}</p>
-          </>
-        );
-      }}
-    </Query>
+          const items = data.welcomes[0];
+          // console.log(items);
+          return (
+            <>
+              <Styled_SiteContainer>
+                <h1>{items.heroHeading}</h1>
+                <p>{items.firstIntroMessage}</p>
+              </Styled_SiteContainer>
+            </>
+          );
+        }}
+      </Query>
+      <MainFooter />
+    </>
   );
 };
 
