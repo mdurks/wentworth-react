6581220be165036591def88455aa80ed8bc129cc
Michael Durkin <mdurks@gmail.com>
2020-07-14 11:00:32 +0100

    Snipcart files added to site.

--- src/App.js
+++ src/App.js
@@ -10,50 +10,68 @@ const MainFooter = loadable(() => import("./components/MainFooter"));
 const Details = loadable(() => import("./Details"));
 const Product_Listings = loadable(() => import("./Product-Listings"));
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <MainHeader />
-      <MainNav />
-      <main className="main">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={loadable(() => import("./Homepage"))}
-          />
-          <Route path="/about" component={loadable(() => import("./About"))} />
+class App extends React.Component {
+  componentDidMount() {
+    var snipcartCSS = document.createElement("link");
+    snipcartCSS.rel = "stylesheet";
+    snipcartCSS.href =
+      "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css";
+    document.head.appendChild(snipcartCSS);
 
-          <Route path="/engagement/rings/:id" component={Details} />
+    var snipcartScript = document.createElement("script");
+    snipcartScript.src =
+      "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js";
+    document.body.appendChild(snipcartScript);
+  }
 
-          <Route path="/weddings/necklaces/:id" component={Details} />
-          <Route path="/weddings/earrings/:id" component={Details} />
-          <Route path="/weddings/rings/:id" component={Details} />
+  render() {
+    return (
+      <BrowserRouter>
+        <MainHeader />
+        <MainNav />
+        <main className="main">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={loadable(() => import("./Homepage"))}
+            />
+            <Route
+              path="/about"
+              component={loadable(() => import("./About"))}
+            />
 
-          <Route path="/jewellery/other/:id" component={Details} />
-          <Route path="/jewellery/bracelets/:id" component={Details} />
-          <Route path="/jewellery/necklaces/:id" component={Details} />
-          <Route path="/jewellery/earrings/:id" component={Details} />
-          <Route path="/jewellery/rings/:id" component={Details} />
+            <Route path="/engagement/rings/:id" component={Details} />
 
-          <Route path="/engagement/:id" component={Product_Listings} />
-          <Route path="/weddings/:id" component={Product_Listings} />
-          <Route path="/jewellery/:id" component={Product_Listings} />
-          <Route
-            path="/jewellery"
-            component={loadable(() => import("./Jewellery"))}
-          />
+            <Route path="/weddings/necklaces/:id" component={Details} />
+            <Route path="/weddings/earrings/:id" component={Details} />
+            <Route path="/weddings/rings/:id" component={Details} />
 
-          <Route
-            path="/contact"
-            component={loadable(() => import("./Contact"))}
-          />
-          <Route component={loadable(() => import("./FourOhFour"))} />
-        </Switch>
-      </main>
-      <MainFooter />
-    </BrowserRouter>
-  );
-};
+            <Route path="/jewellery/other/:id" component={Details} />
+            <Route path="/jewellery/bracelets/:id" component={Details} />
+            <Route path="/jewellery/necklaces/:id" component={Details} />
+            <Route path="/jewellery/earrings/:id" component={Details} />
+            <Route path="/jewellery/rings/:id" component={Details} />
+
+            <Route path="/engagement/:id" component={Product_Listings} />
+            <Route path="/weddings/:id" component={Product_Listings} />
+            <Route path="/jewellery/:id" component={Product_Listings} />
+            <Route
+              path="/jewellery"
+              component={loadable(() => import("./Jewellery"))}
+            />
+
+            <Route
+              path="/contact"
+              component={loadable(() => import("./Contact"))}
+            />
+            <Route component={loadable(() => import("./FourOhFour"))} />
+          </Switch>
+        </main>
+        <MainFooter />
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
